2e27c0d3957d5c90a7f779c11822d3a31874e7bf
Gabriel Rodrigues <gabee_picanco@hotmail.com>
2023-05-28 19:31:55 -0400

    Correção de aceitar varias ações de correções.

--- core/behavior.py
+++ core/behavior.py
@@ -35,41 +35,59 @@ class ExecutarLimpeza:
         return corrigido, nao_corrigido
 
     @staticmethod
-    def __aplica_regra(values, conjunto_de_dados):
+    def __aplica_regra(correto, errado, conjunto_de_dados):
         for regra in conjunto_de_dados.regras:
-            (correto, errado) = ExecutarLimpeza.__regras(regra=regra, dados=conjunto_de_dados.dados)
-            values['correto'] = correto
-            values['errado'] = errado
-        return values
+            (value_correto, value_errado) = ExecutarLimpeza.__regras(regra=regra, dados=conjunto_de_dados.dados)
+            correto.extend(value_correto)
+            errado.extend(value_errado)
+        return correto, errado
 
     @staticmethod
-    def __aplica_acao_de_correcao(values, conjunto_de_dados):
-        dados_de_correcao = values['errado'] if conjunto_de_dados.regras else conjunto_de_dados.dados
+    def __aplica_acao_de_correcao(errado, conjunto_de_dados):
+        corrigido = []
+        nao_corrigido = errado if conjunto_de_dados.regras else conjunto_de_dados.dados
 
         for acao_correcao in conjunto_de_dados.acoes_correcoes:
-            (corrigido, nao_corrigido) = ExecutarLimpeza.__acao_correcao(acao_de_correcao=acao_correcao,
-                                                                         dados=dados_de_correcao)
+            (values_corrigido, values_nao_corrigido) = ExecutarLimpeza.__acao_correcao(acao_de_correcao=acao_correcao,
+                                                                         dados=nao_corrigido)
             # Adiciona os valores corrigidos a lista de correto
-            values['correto'].extend(corrigido)
+            corrigido.extend(values_corrigido)
 
             # Remove valores correspondentes da lista 'errado'
             valores_a_remover = {item[0] for item in corrigido if item[0] is not None}
-            values['errado'] = [item for item in values['errado'] if item[0] not in valores_a_remover]
-        return values
+            nao_corrigido = [item for item in errado if item[0] not in valores_a_remover]
+        return corrigido, nao_corrigido
 
     @staticmethod
     def __ordena_dados(values):
         values['correto'] = sorted(values['correto'], key=lambda x: x[0])
         values['errado'] = sorted(values['errado'], key=lambda x: x[0])
+        values['corrigido'] = sorted(values['corrigido'], key=lambda x: x[0])
+        values['nao_corrigido'] = sorted(values['nao_corrigido'], key=lambda x: x[0])
+        values['dados'] = sorted(values['dados'], key=lambda x: x[0])
 
     @staticmethod
     def run(conjunto_de_dados: ConjuntoDeDados):
-        values = {}
+        correto = []
+        errado = []
+        corrigido = []
+        nao_corrigido = []
+
         if conjunto_de_dados.regras:
-            ExecutarLimpeza.__aplica_regra(values, conjunto_de_dados)
+            correto, errado = ExecutarLimpeza.__aplica_regra(correto, errado, conjunto_de_dados)
 
         if conjunto_de_dados.acoes_correcoes:
-            ExecutarLimpeza.__aplica_acao_de_correcao(values, conjunto_de_dados)
+            corrigido, nao_corrigido = ExecutarLimpeza.__aplica_acao_de_correcao(errado, conjunto_de_dados)
+
+        dados = correto + corrigido + nao_corrigido
+
+        values = {
+            'correto': correto,
+            'errado': errado,
+            'corrigido': corrigido,
+            'nao_corrigido': nao_corrigido,
+            'dados': dados
+        }
 
         if values:
             ExecutarLimpeza.__ordena_dados(values)
